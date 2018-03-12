c7f83ed6bad45293ef1e504667bae7b46a31fbe1
RossPin <dorpetz@hotmail.com>
2018-03-12 21:27:06 +1300

    add eloquentJS C3 exercise solutions

---
+++ eloquent.js
@@ -0,0 +1,51 @@
+//Exercise: Minimum
+function min(a,b){
+  if (a <= b) return a;
+  return b;
+ }
+ 
+ console.log(min(0, 10));
+ // → 0
+ console.log(min(0, -10));
+ // → -10
+
+
+
+//Exercise: Recursion
+function isEven(n) {
+  if (n < 0) n *= -1;
+  if (n === 0) return true;
+  else if (n === 1) return false;
+  else return isEven(n-2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → false
+
+
+
+//Exercise: Bean Counting
+function countBs(str){
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === "B") count++;
+  }
+  return count;
+}
+
+function countChar(str, target){
+  var count = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] === target) count++;
+  }
+  return count;
+}
+
+console.log(countBs("BBC"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
